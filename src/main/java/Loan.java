54a453ef3b1c39948029c9b4dd03cb43056810d1
Justin <hoggersoh@gmail.com>
2024-01-26 17:35:10 +0800

    Revert "Auto stash before checking out "HEAD""
    This reverts commit 8e6fd8660af7b3fb4c7cb88fe6bca28fa5036552.

--- src/main/java/Loan.java
+++
@@ -1,8 +0,0 @@
-public class loan{
-    private int amount;
-    private String borrower;
-    private boolean isPaid;
-    private double interestRate;
-    private String contactNumber;
-
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,11 @@ public class Main {
     static boolean[] isPaid = new boolean[100];
     static double[] interestRates = new double[100];
     static int loansCount = 0;
-    static String[] contactNumbers = new String[100];
 
-    public static void addLoan(String borrower, int amount, double interestRate, String contactNumber) {
+    public static void addLoan(String borrower, int amount, double interestRate) {
         borrowers[loansCount] = borrower;
         amounts[loansCount] = amount;
         interestRates[loansCount] = interestRate;
-        contactNumbers[loansCount] = contactNumber;
         loansCount++;
     }
 
@@ -48,6 +46,5 @@ public class Main {
         setAsPaid("Ben Chee");
         listLoans();
         System.out.println("profit: " + calculateProfitEarned());
-        Loan newLoan = new Loan()
     }
 }
