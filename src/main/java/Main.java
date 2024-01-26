a31834202c48f85f69ab868453f943b1dc490a21
Justin <hoggersoh@gmail.com>
2024-01-26 17:43:33 +0800

    Make variable public

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,34 +15,6 @@ public class Main {
         m2.setAsPaid("Alice Betsy");
         m2.listLoans();
         System.out.println("profit from m2: " + m2.calculateProfitEarned());
-    }
 
-    public static void listLoans() {
-        for (int i = 0; i < loansCount; i++) {
-            if (!isPaid[i]) {
-                System.out.println(borrowers[i] + ": " + amounts[i]);
-            }
-        }
-    }
-
-    public static double calculateProfitEarned() {
-        double profit = 0;
-        for (int i = 0; i < loansCount; i++) {
-            // if the loan was repaid, add the interest to profit
-            if (isPaid[i]) {
-                profit = profit + (amounts[i] * interestRates[i]);
-            }
-        }
-        return profit;
-    }
-
-    public static void main(String[] args) {
-        addLoan("Alice Betsy", 100, 0.1);
-        addLoan("Ben Chee", 50, 0.2);
-        addLoan("Chris Davis", 30, 0.3);
-        setAsPaid("Ben Chee");
-        listLoans();
-        System.out.println("profit: " + calculateProfitEarned());
-        Loan newLoan = new Loan();
     }
 }
